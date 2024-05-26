cf703f348a3a9810e56e38ffc5fbb10a2f661a86
Muhamad Rafi Ramdhani <muhamadrafiramdhani15@gmail.com>
2024-05-26 14:16:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnSubmit = document.getElementById("btnSubmit");
+    
+    btnSubmit.addEventListener('click', function(){
+        alert("Thank youu! I'll contact you very soon");
+    });
+    
